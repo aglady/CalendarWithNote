657d7b42bb3e91c6160c3d9f50b5262eec25dc8b
mpershag <mpershag@IMM-14-512-03C>
2026-03-18 09:58:34 +0300

    Добавьте файлы проекта.

---
+++ CalendarWithNote/CalendarViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarWithNote
+{
+    internal class CalendarViewModel : INotifyPropertyChanged
+    {
+        private DateTime currentMonth;
+        private List<DateModel> dates;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CalendarViewModel()
+        {
+            currentMonth = DateTime.Now;
+            LoadCalendar();
+        }
+
+        public DateTime CurrentMonth
+        {
+            get { return currentMonth; }
+            set
+            {
+                currentMonth = value;
+                LoadCalendar();
+                OnPropertyChanged(nameof(CurrentMonth));
+            }
+        }
+        public List<DateModel> Dates
+        {
+            get { return dates; }
+            set
+            {
+                dates = value;
+                OnPropertyChanged(nameof(Dates));
+            }
+        }
+        private void LoadCalendar()
+        {
+            var daysInMonth = DateTime.DaysInMonth(currentMonth.Year, currentMonth.Month);
+            var startDay = new DateTime(currentMonth.Year, currentMonth.Month, 1).DayOfWeek;
+
+            var calendarDates = new List<DateModel>();
+            
+            for (var day = 1; day <= daysInMonth; day++)
+            {
+                var date = new DateModel()
+                {
+                    Day = new DateTime(currentMonth.Year, currentMonth.Month, day),
+                    WeekDay = (DayOfWeek)(((int)startDay + day - 1) % 7)
+                };
+                calendarDates.Add(date);
+            }
+            Dates = calendarDates;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ CalendarWithNote/DateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarWithNote
+{
+    internal class DateModel
+    {
+        public DateTime Day { get; set; }
+        public DayOfWeek WeekDay { get; set; }
+
+       // public bool HasNote { get; set; }
+    }
+}
